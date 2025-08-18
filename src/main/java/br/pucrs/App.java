62ab1471c06fc3fb1fcbcf5b3386232bf22155f4
gustavoburgie <gustavoburgie@gmail.com>
2025-08-18 20:22:32 -0300

    fazendo o mergeSort

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -1,4 +1,5 @@
 package br.pucrs;
+import java.util.ArrayList;
 
 /**
  * Hello world!
@@ -6,8 +7,61 @@ package br.pucrs;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        System.out.println("Hello World!");
+    }
+
+    /*
+     * Assinatura: int[] mergeSort(int[] vet)
+     * 
+     * Casos base:
+     * se arr tem 1 elemento ou menos
+     * retorna arr;
+     * 
+     * se não
+     * adiciona primeira metade de L em vet;
+     * adiciona a segunda metade de L em vet;
+     * bubble sort vet
+     * retorna vet;
+     * 
+     */
+    public static int[] mergeSort(int[] vet){
+        if(vet.length <= 1)
+        return vet;
+
+        if(vet.length == 2){
+            if(vet[0] > vet[1]){
+                int t = vet[0];
+                vet[0] = vet[1];
+                vet[1] = t;
+            }
+        }
+        else{ //separa vet em 2
+            int vet2[] = null;
+            for(int i = 0; i<=vet.length/2; i++){
+                vet2[i] = vet[i];
+            }
+            int vet3[] = null;
+            int j = 0;
+            for(int i = vet.length; i>vet.length/2; i--){
+                vet3[j] = vet[i];
+                j++;
+            }
+            vet2 = mergeSort(vet2); //primeira metade
+            vet3 = mergeSort(vet3); //segunda metade
+            for(int i = 0; i <= vet.length; i++){
+            }
+
+        }
+
+        return null;
+    }
+
+    //sup pro mergeSort
+    public static int[] merge(int[] v1, int[] v2){
+        
+        
+        return null;
     }
 }
