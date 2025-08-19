d5620bc898247383283de8c70222224ded7c0a7d
gustavoburgie <gustavoburgie@gmail.com>
2025-08-18 22:30:42 -0300

    mergeSort implementado com sucesso

--- src/main/java/br/pucrs/App.java
+++ src/main/java/br/pucrs/App.java
@@ -10,19 +10,45 @@ public class App
     public static void main(String[] args)
     {
         System.out.println("Hello World!");
+        int[] vetor = {1,67,4,13};
+        int[] vetor2 = {1,2,3,4};
+        int[] res = merge(vetor, vetor2);
+        for(int i = 0; i<res.length; i++){
+            if(i==res.length-1){
+                System.out.print(res[i]);
+            }
+            else
+            System.out.print(res[i]+",");
+        }
+        System.out.println();
+        int[] vetres = {1,67,4,13,8,25,12,2,3,4,44};
+        vetres = mergeSort(vetres);
+        for(int i = 0; i<vetres.length; i++){
+            if(i==vetres.length-1){
+                System.out.print(vetres[i]);
+            }
+            else
+            System.out.print(vetres[i]+",");
+        }
     }
 
     /*
      * Assinatura: int[] mergeSort(int[] vet)
      * 
      * Casos base:
-     * se arr tem 1 elemento ou menos
-     * retorna arr;
+     * se vet tem 1 elemento ou menos
+     * retorna vet;
+     * 
+     * se vet tem 2 elementos
+     * if(primeiro > segundo)
+     * troca eles de lugar
+     * return vet;
      * 
      * se não
-     * adiciona primeira metade de L em vet;
-     * adiciona a segunda metade de L em vet;
-     * bubble sort vet
+     * vet1 = mergeSort(primeira metade de vet);
+     * vet2 = mergeSort(segunda metade de vet);
+     * vet = vet1 + vet2;
+     * bubble sort de vet;
      * retorna vet;
      * 
      */
@@ -36,32 +62,54 @@ public class App
                 vet[0] = vet[1];
                 vet[1] = t;
             }
+            return vet;
         }
         else{ //separa vet em 2
-            int vet2[] = null;
-            for(int i = 0; i<=vet.length/2; i++){
+            int vet2[] = new int[vet.length/2];
+            for(int i = 0; i<vet.length/2; i++){
                 vet2[i] = vet[i];
             }
-            int vet3[] = null;
+            int vet3[] = new int[vet.length/2 + vet.length%2];
             int j = 0;
-            for(int i = vet.length; i>vet.length/2; i--){
+            for(int i = vet.length/2; i<vet.length; i++){
                 vet3[j] = vet[i];
                 j++;
             }
             vet2 = mergeSort(vet2); //primeira metade
             vet3 = mergeSort(vet3); //segunda metade
-            for(int i = 0; i <= vet.length; i++){
-            }
-
+            vet = merge(vet2, vet3); //junta os dois organizando
+            return vet;
         }
-
-        return null;
     }
 
     //sup pro mergeSort
     public static int[] merge(int[] v1, int[] v2){
-        
-        
-        return null;
+        int tam = v1.length + v2.length;
+        int[] merged = new int[tam];
+        int i = 0;
+        //junta os dois vetores no merged
+        for(int j = 0; j < v1.length; j++){
+            merged[i] = v1[j];
+            i++;
+        }
+        for(int j = 0; j < v2.length; j++){
+            merged[i] = v2[j];
+            i++;
+        }
+        //bubble sort
+        for(int j = merged.length-1; j>=0; j--){
+            for(int k = 0; k<j; k++){
+                if(merged[k] > merged[k+1]){
+                    int t = merged[k];
+                    merged[k] = merged[k+1];
+                    merged[k+1] = t;
+                }
+            }
+        }
+        return merged;
+    }
+
+    public static long maxVal1(long A[], int init, int end){
+        return 0;
     }
 }
